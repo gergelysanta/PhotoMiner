7ac4f1c1a8b13b91f7511303e6315fd0b090becb
Gergely Sánta <gergely.santa@trikatz.com>
2016-12-07 13:23:02 +0100

    Initial commit

---
+++ PhotoMiner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoMiner
+//
+//  Created by Gergely Sánta on 07/12/2016.
+//  Copyright © 2016 TriKatz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ PhotoMiner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhotoMiner
+//
+//  Created by Gergely Sánta on 07/12/2016.
+//  Copyright © 2016 TriKatz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
